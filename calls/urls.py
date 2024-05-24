57f09eb265279c9c6a2fd9e6a5bb916e49502f9c
Gauravjot Garaya <gauravjot@outlook.com>
2024-05-23 22:08:16 -0700

    added twilio endpoints

--- calls/urls.py
+++ calls/urls.py
@@ -6,4 +6,6 @@ api/phone_call/
 """
 urlpatterns = [
     path('make/', views.make_call),
+    path('token/', views.grant_twilio_token),
+    path('twiml/', views.twiml)
 ]
--- calls/views.py
+++ calls/views.py
@@ -7,9 +7,10 @@ from utils.error_handling.error_message import ErrorMessage
 from decouple import config
 from core.middlewares import is_web
 # Twilio
+from twilio import twiml
 from twilio.rest import Client
-# from twilio.jwt.access_token import AccessToken
-# from twilio.jwt.access_token.grants import VoiceGrant
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VoiceGrant
 # User Imports
 from users.permissions import HasSessionOrTokenActive
 from users.api import get_request_user
@@ -23,14 +24,6 @@ def make_call(request):
     auth_token = config('TWILIO_AUTH_TOKEN')
     client = Client(account_sid, auth_token)
 
-    # token = AccessToken(twilio_account_sid, twilio_api_key_sid,
-    #                     twilio_api_key_secret, identity=get_request_user(
-    #                         request).email,
-    #                     ttl=3600)
-
-    # # add grants to token
-    # token.add_grant(VoiceGrant(room='My Room'))
-
     call = client.calls.create(
         url="http://demo.twilio.com/docs/voice.xml",
         to=phone_number,
@@ -38,3 +31,37 @@ def make_call(request):
     )
 
     return JsonResponse({"call_id": call.sid}, status=200, safe=False)
+
+
+@api_view(['GET'])
+@permission_classes([HasSessionOrTokenActive])
+def grant_twilio_token(request):
+    account_sid = config('TWILIO_ACCOUNT_SID')
+    api_key = config('TWILIO_API_KEY')
+    api_secret = config('TWILIO_API_KEY_SECRET')
+
+    token = AccessToken(account_sid, api_key, api_secret,
+                        identity=get_request_user(request).email,
+                        ttl=3600)
+
+    # add grants to token
+    token.add_grant(VoiceGrant(incoming_allow=True,
+                    push_credential_sid=config('TWILIO_PUSH_CREDENTIAL_SID'),
+                    outgoing_application_sid=config(
+                        'TWILIO_OUTGOING_APPLICATION_SID')
+    ))
+
+    return JsonResponse({"token": token.to_jwt()}, status=200, safe=False)
+
+
+@api_view(['POST'])
+@permission_classes([HasSessionOrTokenActive])
+def twiml(request):
+    voice_response = twiml.VoiceResponse()
+
+    if not hasattr(request.data, 'to'):
+        return Response(ErrorMessage('to is required'), status=400)
+
+    print(request.data)
+
+    return Response(status=400)
